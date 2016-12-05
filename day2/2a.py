69f340acfd26c290a13f39f62807f673567bc80f
David Harris <dharr419@ford.com>
2016-12-04 23:29:55 -0500

    2a completed

---
+++ day2/2a.py
@@ -0,0 +1,20 @@
+numpad = [[1,2,3],[4,5,6],[7,8,9]]
+key = [1,1]
+
+def getNumber(line, key):
+    for letter in line:
+        if letter == "U":
+            key[1] = max(0, key[1] - 1)
+        elif letter == "D":
+            key[1] = min(2, key[1] + 1)
+        elif letter == "L":
+            key[0] = max(0, key[0] - 1)
+        elif letter == "R":
+            key[0] = min(2, key[0] + 1)
+    return key
+
+# Parse input and get code
+with open('day2.txt', 'r') as f:
+    for line in f:
+        key = getNumber(line, key)
+        print numpad[key[1]][key[0]]
