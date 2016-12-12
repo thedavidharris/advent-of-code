cf96f43c8bc7bbd3692cd5f5024b33b9cf07d5fd
David Harris <dharr419@ford.com>
2016-12-12 11:11:31 -0500

    12a finished

---
+++ day12/12a.py
@@ -0,0 +1,34 @@
+registers = {
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0
+}
+
+def readRegister(x):
+    try:
+        return int(x)
+    except:
+        return registers[x]
+
+with open('day12.txt', 'r') as f:
+    instructions = f.readlines()
+instructions = [x.strip().split() for x in instructions]
+
+ip = 0
+
+while ip < len(instructions):
+    instruction = instructions[ip]
+    if instruction[0] == "cpy":
+        registers[instruction[2]] = readRegister(instruction[1])
+    if instruction[0] == "inc":
+        registers[instruction[1]] += 1
+    if instruction[0] == "dec":
+        registers[instruction[1]] -= 1
+    if instruction[0] =="jnz":
+        if readRegister(instruction[1]):
+            ip += readRegister(instruction[2]) - 1
+
+    ip += 1
+
+print registers['a']
