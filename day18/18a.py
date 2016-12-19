22136a8c560a208395c7dc01139b0b15a4c87c63
David Harris <dharr419@ford.com>
2016-12-19 11:04:04 -0500

    18 done

---
+++ day18/18a.py
@@ -0,0 +1,10 @@
+with open('day18.txt', 'r') as f:
+    input = f.readline()
+
+row = [x == "." for x in input]
+numSafe = sum(row)
+for _ in range (1, 40):
+    row = [True] + row + [True]
+    row = [left == right for left, right in zip(row, row[2:])]
+    numSafe += sum(row)
+print numSafe
