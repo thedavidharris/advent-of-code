fc84d6f79b2ebe72a0ea782b86d17016d8a9fda9
David Harris <dharr419@ford.com>
2016-12-18 21:04:22 -0500

    17a done

---
+++ day17/17a.py
@@ -0,0 +1,35 @@
+from hashlib import md5
+from itertools import compress
+from Queue import Queue
+from operator import add
+
+# moves using array indexing [y][x]
+moves = {
+    "U": (-1, 0),
+    "D": (1, 0),
+    "L": (0, -1),
+    "R": (0, 1)
+}
+
+def getDoors(path):
+    code = "awrkjxxr"
+    hashValue =  md5(code + "".join(path)).hexdigest()
+    return compress('UDLR', [x in "bcdef" for x in hashValue[:4]])
+
+start = (0,0)
+goal = (3,3)
+frontier = Queue()
+frontier.put((start, []))
+
+while not frontier.empty():
+    currentPos, currentPath = frontier.get()
+    if currentPos == goal:
+        break
+    for direction in getDoors(currentPath):   
+        nextPos = tuple(map(add, currentPos, moves[direction]))
+        if (0 <= nextPos[0] <= 3) and (0 <= nextPos[1] <= 3):
+            frontier.put((nextPos, currentPath + [direction]))
+
+print "".join(currentPath)
+
+#print "DDUDRLRRUDRD"
