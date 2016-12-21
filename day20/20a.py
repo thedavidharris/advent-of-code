bcb421d06b457ddbf8c47c6ca33a2ad9262c1f96
David Harris <dharr419@ford.com>
2016-12-21 01:55:14 -0500

    20a down to 3 lines

--- day20/20a.py
+++ day20/20a.py
@@ -1,13 +1,3 @@
 with open('day20.txt', 'r') as f:
-    input = f.readlines()
-ranges = [map(int, x.strip().split("-")) for x in input]
-
-sortedRanges = zip(sorted([column[0] for column in ranges]) + [2**32], [0] + sorted([column[1] for column in ranges]))
-
-min = 2**32
-for low, high in sortedRanges:
-    if low > high + 1:
-        min = high + 1
-        break
-
-print min
+    ranges = [map(int, x.strip().split("-")) for x in f.readlines()]
+print filter(lambda range: range[0] > range[1] + 1, zip(sorted([column[0] for column in ranges]) + [2**32], [0] + sorted([column[1] for column in ranges])))[0][1] + 1
