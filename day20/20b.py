e10777d3e8b650536de1a8f8eaa828d168dcccdd
David Harris <dharr419@ford.com>
2016-12-21 02:04:35 -0500

    20b down to 3 lines

--- day20/20b.py
+++ day20/20b.py
@@ -1,12 +1,3 @@
 with open('day20.txt', 'r') as f:
-    input = f.readlines()
-ranges = [map(int, x.strip().split("-")) for x in input]
-
-sortedRanges = zip(sorted([column[0] for column in ranges]) + [2**32], [0] + sorted([column[1] for column in ranges]))
-
-sum = 0
-for low, high in sortedRanges:
-    if low > high + 1:
-        sum += low - high - 1
-
-print sum
+    ranges = [map(int, x.strip().split("-")) for x in f.readlines()]
+print reduce(lambda x,y: x+y, map(lambda x: x[0] - x[1] - 1, filter(lambda range: range[0] > range[1] + 1, zip(sorted([column[0] for column in ranges]) + [2**32], [0] + sorted([column[1] for column in ranges])))))
